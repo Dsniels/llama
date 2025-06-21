788e7ca72e4589e576be46b043151eef5db5e3fa
Daniel Salazar <dsnile.salazr@gmail.com>
2025-06-20 23:42:12 -0600

    setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,53 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+/** @format */
 
-import { routes } from './app.routes';
+import {
+	ApplicationConfig,
+	inject,
+	provideAppInitializer,
+	provideBrowserGlobalErrorListeners,
+	provideZonelessChangeDetection,
+} from "@angular/core";
+import {provideRouter} from "@angular/router";
+import {
+	MenuFoldOutline,
+	MenuUnfoldOutline,
+	FormOutline,
+	DashboardOutline,
+} from "@ant-design/icons-angular/icons";
+import {routes} from "./app.routes";
+import {NzConfig, provideNzConfig} from "ng-zorro-antd/core/config";
+import {provideNzIcons} from "ng-zorro-antd/icon";
+import {en_US, provideNzI18n} from "ng-zorro-antd/i18n";
+import {Websocket} from "./services/websocket/services/websocket";
+import { provideStore } from "@ngrx/store";
+import { provideEffects } from "@ngrx/effects";
+import { ChatEffects } from "./store/chat/chat.effects";
+import { reducers } from "./store";
+const ngZorroConfig: NzConfig = {
+	message: {nzTop: 120},
+	notification: {nzTop: 240},
+};
+
+const onInit = () => {
+	const ws = inject(Websocket);
+	// ws.startAsync();
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+	providers: [
+		provideAppInitializer(onInit),
+		provideBrowserGlobalErrorListeners(),
+		provideStore(reducers),
+		provideEffects([ChatEffects]),
+		provideZonelessChangeDetection(),
+		provideRouter(routes),
+		provideNzI18n(en_US),
+		provideNzConfig(ngZorroConfig),
+		provideNzIcons([
+			MenuFoldOutline,
+			MenuUnfoldOutline,
+			DashboardOutline,
+			FormOutline,
+		]),
+	],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NzBreadCrumbModule} from "ng-zorro-antd/breadcrumb"
+import {NzIconModule} from "ng-zorro-antd/icon"
+import {NzLayoutModule} from "ng-zorro-antd/layout"
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NzIconModule, NzBreadCrumbModule, NzLayoutModule, NzMenuModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
